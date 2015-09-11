572a1e6ef78351fb8478928d5675b16a5ff92ae3
Mariel Van Norman <melvinopolis@gmail.com>
2015-09-11 11:56:29 -0400

    adds mobile layout for home page and adds about page

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
 	// load navbar and footer
+    $('head').load('head.html')
     $('#navbar').load("navbar.html");
     $('#footer').load("footer.html");
 
     // toggle active navbar link. doesn't work yet!
-    var activeLink = function(){
-    	$('.nav-links li').click(function(e){
-    		e.preventDefault();
-    		e.stopPropagation();
-    		$(this).siblings().removeClass('active');
-    		$(this).addClass('active');
-    	});
-    };
+    // var activeLink = function(){
+    // 	$('.nav-links li').click(function(e){
+    // 		e.preventDefault();
+    // 		e.stopPropagation();
+    // 		$(this).siblings().removeClass('active');
+    // 		$(this).addClass('active');
+    // 	});
+    // };
     	
 
     // toggle info boxes on home page
